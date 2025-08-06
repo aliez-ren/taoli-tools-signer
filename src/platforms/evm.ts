43cc39f55472e4e58f198aa778764c23f1a3cee9
Aliez <8287771+aliez-ren@users.noreply.github.com>
2025-08-06 21:42:06 +0900

    Update evm.ts

--- src/platforms/evm.ts
+++ src/platforms/evm.ts
@@ -84,77 +84,74 @@ export const EVM: Platform<Address> = async (mnemonic, passphrase) => {
 }
 
 const allowlist: Record<Address, Set<number>> = {
-  // https://web3.okx.com/build/dev-docs/dex-api/dex-get-aggregator-supported-chains
-  '0x40aA958dd87FC8305b97f2BA922CDdCa374bcD7f': new Set([
-    avalanche.id,
-    ethereum.id,
-  ]),
-  '0x3B86917369B83a6892f553609F3c2F439C184e31': new Set([polygon.id]),
-  '0x70cBb871E8f30Fc8Ce23609E9E0Ea87B6b222F58': new Set([
-    arbitrum.id,
-    cronos.id,
-    fantom.id,
-    okc.id,
-  ]),
-  '0x2c34A2Fb1d0b4f55de51E1d0bDEfaDDce6b7cDD6': new Set([bsc.id]),
-  '0x68D6B739D2020067D1e2F713b999dA97E4d54812': new Set([
-    confluxESpace.id,
+  // https://web3.okx.com/build/dev-docs/dex-api/dex-smart-contract#dex-router
+  '0xDd5E9B947c99Aa60bab00ca4631Dce63b49983E7': new Set([ethereum.id]),
+  '0x8feB9E84b7E9DC86adc6cD6Eb554C5B4355c8405': new Set([sonic.id, mode.id]),
+  '0x108bfC6aa49718a1f9669FC0Ccd1a21d0D30c8E0': new Set([zksync.id]),
+  '0x06f183D52D92c13a5f2B989B8710BA7F00bd6f87': new Set([
     optimism.id,
-  ]),
-  '0xc67879F4065d3B9fe1C09EE990B891Aa8E3a4c2f': new Set([zksync.id]),
-  '0x57df6092665eb6058DE53939612413ff4B09114E': new Set([
-    base.id,
-    linea.id,
-    manta.id,
-    mantle.id,
-    metis.id,
-    polygonZkEvm.id,
     scroll.id,
   ]),
-  '0x8b773D83bc66Be128c60e07E17C8901f7a64F000': new Set([merlin.id, xLayer.id]),
-  '0x03B5ACdA01207824cc7Bc21783Ee5aa2B8d1D2fE': new Set([zetachain.id]),
-  '0x5fD2Dc91FF1dE7FF4AEB1CACeF8E9911bAAECa68': new Set([blast.id]),
+  '0x6733Eb2E75B1625F1Fe5f18aD2cB2BaBDA510d19': new Set([
+    polygon.id,
+    okc.id,
+    avalanche.id,
+    mantle.id,
+    blast.id,
+    merlin.id,
+    xLayer.id,
+    sei.id,
+  ]),
+  '0xA2604105e84DAe6eccEa41E61123AB8d9B3AfD75': new Set([bsc.id]),
+  '0x79f7C6C6dc16Ed3154E85A8ef9c1Ef31CEFaEB19': new Set([fantom.id, cronos.id]),
+  '0x69C236E021F5775B0D0328ded5EaC708E3B869DF': new Set([
+    arbitrum.id,
+    confluxESpace.id,
+    base.id,
+    polygonZkEvm.id,
+  ]),
+  '0xcF76984119C7f6ae56fAfE680d39C08278b7eCF4': new Set([linea.id]),
+  '0x7A7AD9aa93cd0A2D0255326E5Fb145CEc14997FF': new Set([
+    manta.id,
+    zetachain.id,
+  ]),
+  '0xC589a4eD6A9fc3354d7eeF88bA87b51AFC272783': new Set([metis.id]),
+  '0x9FD43F5E4c24543b2eBC807321E58e6D350d6a5A': new Set([unichain.id]),
+
+  // https://web3.okx.com/build/dev-docs/dex-api/dex-smart-contract#token-approval
+  '0x40aA958dd87FC8305b97f2BA922CDdCa374bcD7f': new Set([
+    ethereum.id,
+    avalanche.id,
+  ]),
   '0xd321ab5589d3e8fa5df985ccfef625022e2dd910': new Set([sonic.id]),
+  '0xc67879F4065d3B9fe1C09EE990B891Aa8E3a4c2f': new Set([zksync.id]),
+  '0x68D6B739D2020067D1e2F713b999dA97E4d54812': new Set([
+    optimism.id,
+    confluxESpace.id,
+  ]),
+  '0x3B86917369B83a6892f553609F3c2F439C184e31': new Set([polygon.id]),
+  '0x2c34A2Fb1d0b4f55de51E1d0bDEfaDDce6b7cDD6': new Set([bsc.id]),
+  '0x70cBb871E8f30Fc8Ce23609E9E0Ea87B6b222F58': new Set([
+    okc.id,
+    fantom.id,
+    arbitrum.id,
+    cronos.id,
+  ]),
+  '0x57df6092665eb6058DE53939612413ff4B09114E': new Set([
+    linea.id,
+    base.id,
+    mantle.id,
+    scroll.id,
+    manta.id,
+    metis.id,
+    polygonZkEvm.id,
+  ]),
+  '0x5fD2Dc91FF1dE7FF4AEB1CACeF8E9911bAAECa68': new Set([blast.id]),
+  '0x03B5ACdA01207824cc7Bc21783Ee5aa2B8d1D2fE': new Set([zetachain.id]),
+  '0x8b773D83bc66Be128c60e07E17C8901f7a64F000': new Set([merlin.id, xLayer.id]),
   '0xbd0EBE49779E154E5042B34D5BcfBc498e4B3249': new Set([mode.id]),
   '0x801D8ED849039007a7170830623180396492c7ED': new Set([sei.id]),
   '0x2e28281Cf3D58f475cebE27bec4B8a23dFC7782c': new Set([unichain.id]),
-
-  // https://web3.okx.com/zh-hans/build/dev-docs/dex-api/dex-smart-contract
-  '0x6088d94C5a40CEcd3ae2D4e0710cA687b91c61d0': new Set([
-    ethereum.id,
-    arbitrum.id,
-  ]),
-  '0x9b9efa5Efa731EA9Bbb0369E91fA17Abf249CFD4': new Set([
-    avalanche.id,
-    base.id,
-    bsc.id,
-    manta.id,
-    mode.id,
-    optimism.id,
-    polygon.id,
-    scroll.id,
-    sonic.id,
-    zetachain.id,
-  ]),
-  '0x5058C498864795689fe41fB54f29a8B71F0A7201': new Set([zksync.id]),
-  '0xd30D8CA2E7715eE6804a287eB86FAfC0839b1380': new Set([
-    okc.id,
-    fantom.id,
-    mantle.id,
-    blast.id,
-    polygonZkEvm.id,
-    merlin.id,
-    xLayer.id,
-    mode.id,
-    sei.id,
-  ]),
-  '0x06f183D52D92c13a5f2B989B8710BA7F00bd6f87': new Set([linea.id, metis.id]),
-  '0x8feB9E84b7E9DC86adc6cD6Eb554C5B4355c8405': new Set([confluxESpace.id]),
-  '0xA3d7C702e6Fa835504B4a9649F422d1DdC6995E3': new Set([unichain.id]),
-  '0xbC92122fDa4061FF3f6C068d2cc7e6c0bd81bA00': new Set([ethereum.id]),
-  '0xD59d6b26C2593ED0D56bA6170a449dF1e394B65A': new Set([base.id]),
-  '0x76D43a252300955E6bd17A6992201fc02f0E5C10': new Set([bsc.id]),
-  '0x0b59f6798c467136819f2B276fa03032cC0A4653': new Set([arbitrum.id]),
 
   // https://portal.1inch.dev/documentation/contracts/aggregation-protocol/aggregation-introduction
   '0x111111125421cA6dc452d289314280a0f8842A65': new Set([
